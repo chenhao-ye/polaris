3604955e55fbc46c285a0e06d17ebf0aa65ff144
ScarletGuo <zguo74@wisc.edu>
2019-12-06 00:31:28 -0600

    changed to queue

--- concurrency_control/row_clv.cpp
+++ concurrency_control/row_clv.cpp
@@ -127,10 +127,10 @@ RC Row_clv::lock_release(txn_man * txn) {
 		pthread_mutex_lock( latch );
 
 	// Try to find the entry in the retired
-	txn_man * prev_retired = retired->txn;
+	txn_man * prev_retired = retired;
 	LockEntry * en = remove_if_exists(retired, txn, false);
 	if (en != NULL) {
-        if ((retired != NULL) && (retired->txn != prev_retired)) {
+        if ((retired != NULL) && (retired != prev_retired)) {
             // becoming head always remove one barrier
             retired->txn->decrement_commit_barriers();
         }
