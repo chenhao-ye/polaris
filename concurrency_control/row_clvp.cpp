31eb52a58120998c2439f1992f73102bdf27c2ba
scarletg <scarletg@node-2.clv.spark-pg0.wisc.cloudlab.us>
2019-12-08 20:46:45 -0700

    switch working station

--- concurrency_control/row_clvp.cpp
+++ concurrency_control/row_clvp.cpp
@@ -463,7 +463,6 @@ CLVLockEntry *
 Row_clvp::remove_if_exists_in_owner(txn_man * txn) {
 	CLVLockEntry * en = owners;
 	CLVLockEntry * prev = NULL;
-	CLVLockEntry * found;
 	CLVLockEntry * to_return = NULL;
 
 	while (en != NULL) {
--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
-rm debug-1.out
+rm debug.out
 cp -r config_tpcc_debug.h config.h
-#cp -r config_real.h config.h
-wl='tpcc'
-#wl='ycsb'
-#python test_debug.py ${wl} "CLV,NO_WAIT,WOUND_WAIT,WAIT_DIE" |& tee debug-1.out
-python test_debug.py ${wl} "CLV" |& tee -a debug-1.out
-python test_debug.py ${wl} "NO_WAIT" |& tee -a debug-1.out
-python test_debug.py ${wl} "WOUND_WAIT" |& tee -a debug-1.out
-python test_debug.py ${wl} "WAIT_DIE" |& tee -a  debug-1.out
+
+wl="TPCC"
+threads=3
+cnt=10000
+wh=1
+penalty=10
+
+#for alg in "NO_WAIT" "WAIT_DIE" "WOUND_WAIT" "CLV"
+for alg in "CLV"
+do
+	timeout 300 python test_debug.py ${wl} $alg $threads $cnt $penalty $wh |& tee  debug.out
+done
