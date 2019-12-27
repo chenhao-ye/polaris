108f82705879ff02ad1eaa78b9058c1b1f3fc502
scarletg <scarletg@node0.c32.spark-pg0.clemson.cloudlab.us>
2019-12-27 14:23:31 -0500

    done ycsb

---
+++ run_exp.sh
@@ -0,0 +1,3 @@
+./ycsb_test.sh
+./ycsb_test.sh
+./tpcc_test.sh
--- tpcc_test.sh
+++ tpcc_test.sh
@@ -5,7 +5,8 @@ threads=20
 cnt=100000
 penalty=1
 wh=1
-
+for i in {1..3}
+do
 for alg in "CLV" #"WOUND_WAIT" "WAIT_DIE" "NO_WAIT" 
 do
 for wh in 16 8 4 2 1
@@ -15,6 +16,7 @@ do
 for threads in 32 16 8 4 2 1
 do
 	timeout 300 python test.py WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
+done
 done
 done
 done
