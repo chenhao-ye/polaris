d7d345b2f723f8203dff6427399d87a396fd8bc3
scarletg <scarletg@node0.c32.spark-pg0.clemson.cloudlab.us>
2020-01-05 10:48:32 -0500

    tpcc single thread

--- tpcc_debug.sh
+++ tpcc_debug.sh
@@ -2,7 +2,7 @@ cp -r config_tpcc_debug.h config.h
 rm debug.out
 
 wl="TPCC"
-threads=8
+threads=1
 cnt=10000
 penalty=1
 wh=4
@@ -15,3 +15,4 @@ valgrind --tool=callgrind ./rundb
 alg="WOUND_WAIT"
 timeout 5 python test.py WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh} |& tee -a debug.out
 valgrind --tool=callgrind ./rundb
+
