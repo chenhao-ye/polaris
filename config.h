537b9e5c9e5ea07292bb5e00cc4c188c352af5f1
scarletg <scarletg@node-2.cc-test.spark-pg0.wisc.cloudlab.us>
2020-01-09 03:50:19 -0600

    print profiling

--- config.h
+++ config.h
@@ -190,7 +190,7 @@ extern TestCases					g_test_case;
 #define DEBUG_BENCHMARK					false
 #define DEBUG_CLV                   			false	
 #define DEBUG_TMP					false	
-#define DEBUG_PROFILING false
+#define DEBUG_PROFILING true
 
 /***********************************************/
 // Constant
