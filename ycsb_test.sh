c862e78cbddf653ac8c7c4a73492a522c72a81d9
scarletg <scarletg@node-2.cc-test.spark-pg0.wisc.cloudlab.us>
2020-01-09 02:35:33 -0600

    ycsb some results

--- ycsb_test.sh
+++ ycsb_test.sh
@@ -2,19 +2,20 @@ cp -r config_ycsb_debug.h config.h
 
 wl="YCSB"
 threads=20
-cnt=100000
+cnt=10000 #0
 penalty=1
 zipf=0
 synthetic="true"
-table_size="1024*1024*10"
+table_size="1024*1024*20"
+pf="false"
 
 for alg in "CLV" "WOUND_WAIT" "WAIT_DIE" "NO_WAIT" 
 do
 for zipf in 0
 do
-for threads in 1 2 4 8 16 32
+for threads in 1 2 4 8 16 #32
 do
-	timeout 100 python test.py SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
+	timeout 50 python test.py DEBUG_PROFILING=$pf SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
 done
 done
 done
