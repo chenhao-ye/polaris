1d5992477a6ec14060f80d9d6ffb9246c7b12045
scarletg <scarletg@node-2.cc-test.spark-pg0.wisc.cloudlab.us>
2020-01-10 20:01:12 -0600

    ww profiling results

--- ycsb_debug.sh
+++ ycsb_debug.sh
@@ -9,11 +9,11 @@ synthetic="true"
 table_size="1024*1024*20"
 profile="true"
 
-for alg in "CLV" #"WOUND_WAIT" "WAIT_DIE" "NO_WAIT" 
+for alg in "CLV" "WOUND_WAIT" # "WAIT_DIE" "NO_WAIT" 
 do
 for zipf in 0
 do
-for threads in 8 16 # 2 4 8 16 32
+for threads in 1 2 4 8   # 2 4 8 16 32
 do
 	timeout 30 python test.py DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
 done
