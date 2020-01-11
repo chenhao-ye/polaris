4a0115ebce98c5f9388a89fbaa0c0c672dba5d99
ScarletGuo <zguo74@wisc.edu>
2020-01-11 10:25:39 +0800

    ycsb print query

--- ycsb_debug.sh
+++ ycsb_debug.sh
@@ -8,14 +8,15 @@ zipf=0
 synthetic="true"
 table_size="1024*1024*20"
 profile="true"
+req=6
 
-for alg in "CLV" "WOUND_WAIT" # "WAIT_DIE" "NO_WAIT" 
+for alg in "CLV" # "WOUND_WAIT" # "WAIT_DIE" "NO_WAIT" 
 do
 for zipf in 0
 do
-for threads in 1 2 4 8   # 2 4 8 16 32
+for threads in 4 8   # 2 4 8 16 32
 do
-	timeout 30 python test.py DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
+	timeout 30 python test.py REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
 done
 done
 done
