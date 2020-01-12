225e09d91dc410f56914d3d8420abf693aacbc81
ScarletGuo <zguo74@wisc.edu>
2020-01-12 16:22:37 +0800

    processed results and set tpcc exp

--- tpcc_test.sh
+++ tpcc_test.sh
@@ -5,17 +5,18 @@ threads=20
 cnt=100000
 penalty=1
 wh=1
+spin="true"
 for i in {1..3}
 do
-for alg in "CLV" "WOUND_WAIT" "WAIT_DIE" "NO_WAIT" 
+for alg in "CLV" #"WOUND_WAIT" "WAIT_DIE" "NO_WAIT" 
 do
 for wh in 16 8 4 2 1
 do
 for cnt in 1000 100000
 do
-for threads in 32 16 8 4 2 1
+for threads in 16 8 4 2 1
 do
-	timeout 300 python test.py WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
+	timeout 300 python test.py SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
 done
 done
 done
