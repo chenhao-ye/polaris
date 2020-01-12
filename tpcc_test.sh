65707e93cebffc8951acb8a02205cdddcfe2cf10
ScarletGuo <zguo74@wisc.edu>
2020-01-12 20:16:28 +0800

    added profiling

--- tpcc_test.sh
+++ tpcc_test.sh
@@ -6,17 +6,18 @@ cnt=100000
 penalty=1
 wh=1
 spin="true"
+pf="true"
 for i in {1..3}
 do
-for alg in "WOUND_WAIT" "WAIT_DIE" "NO_WAIT" 
+for alg in "CLV" #"WOUND_WAIT" "WAIT_DIE" "NO_WAIT" 
 do
 for wh in 16 8 4 2 1
 do
-for cnt in 1000 100000
+for cnt in 100000
 do
 for threads in 16 8 4 2 1
 do
-	timeout 100 python test.py SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
+	timeout 100 python test.py DEBUG_PROFILING=pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
 done
 done
 done
