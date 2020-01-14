eb45ac502e85d0673be1bf5c564a17554b96cb45
ScarletGuo <zguo74@wisc.edu>
2020-01-14 16:35:27 +0800

    validate correctness for prioritizing

--- ycsb_debug.sh
+++ ycsb_debug.sh
@@ -10,6 +10,8 @@ table_size="1024*1024*10"
 profile="true"
 req=5
 spin="true"
+phs="true"
+on=1
 
-timeout 30 python test.py SPINLOCK=$spin REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
+timeout 30 python test.py PRIORITIZE_HS=$phs CLV_RETIRE_ON=$on SPINLOCK=$spin REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
 
