1effb7261cb1ffef8293b5284f6b2b91caf71f63
ScarletGuo <zguo74@wisc.edu>
2020-01-15 00:22:13 +0800

    may not in waiter due to aborting descendants

--- concurrency_control/row_clvp.cpp
+++ concurrency_control/row_clvp.cpp
@@ -258,7 +258,7 @@ RC Row_clvp::lock_release(txn_man * txn, RC rc) {
 			return_entry(en);
 			bring_next(NULL);
 		} else {
-			assert(rm_if_in_waiters(txn));
+			rm_if_in_waiters(txn);
 		}
 	} else if (owner_cnt == 0) {
 		bring_next(NULL);
