6c347f311b006fd8fc0d0956f216d0153be3aacc
scarletg <scarletg@node-2.cc-test.spark-pg0.wisc.cloudlab.us>
2020-01-16 23:44:32 -0600

    added runlog to analyze abort pattern

--- tpcc_debug.sh
+++ tpcc_debug.sh
@@ -3,6 +3,7 @@ rm debug.out
 
 wl="TPCC"
 threads=16
+threads=8
 cnt=100000
 penalty=1
 wh=1
@@ -16,7 +17,8 @@ phs="false"
 tmp="true"
 tmp="false"
 dynamic="true"
+debug="true"
 
-timeout 50 python test.py DYNAMIC_TS=$dynamic DEBUG_TMP=$tmp PRIORITIZE_HS=$phs CLV_RETIRE_ON=$on CLV_RETIRE_OFF=$off DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
+timeout 20 python test.py DEBUG_CLV=$debug DYNAMIC_TS=$dynamic DEBUG_TMP=$tmp PRIORITIZE_HS=$phs CLV_RETIRE_ON=$on CLV_RETIRE_OFF=$off DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
 
 #timeout 50 python test.py DEBUG_TMP="false" PRIORITIZE_HS=$phs CLV_RETIRE_ON=$on CLV_RETIRE_OFF=$off DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
