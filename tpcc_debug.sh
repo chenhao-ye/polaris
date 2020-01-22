4f7b96fa13d1d70fc332e4aab920146512537919
scarletg <scarletg@node-2.cc-test.spark-pg0.wisc.cloudlab.us>
2020-01-22 11:21:38 -0600

    nodist reordered

--- tpcc_debug.sh
+++ tpcc_debug.sh
@@ -23,7 +23,7 @@ dynamic="false"
 debug="false"
 #debug="true"
 nodist="true"
-#nodist="false"
+nodist="false"
 perc=0.5
 perc=1
 merge="true"
@@ -31,11 +31,17 @@ merge="false"
 reorder="false"
 reorder="true"
 
+for perc in 1 0.5
+do
+for i in 1 2 3
+do
 for alg in CLV WOUND_WAIT WAIT_DIE NO_WAIT
 do
 for threads in 16 8 4 2 1
 do
 timeout 50 python test.py REORDER_WH=$reorder MERGE_HS=$merge PERC_PAYMENT=$perc DEBUG_BENCHMARK=$nodist DEBUG_CLV=$debug DYNAMIC_TS=$dynamic DEBUG_TMP=$tmp PRIORITIZE_HS=$phs CLV_RETIRE_ON=$on CLV_RETIRE_OFF=$off DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
+done
+done
 done
 done
 
