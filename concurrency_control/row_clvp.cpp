867579f737973c8a0927ec2708b86f18452773e1
ScarletGuo <zguo74@wisc.edu>
2020-01-27 12:30:22 -0600

    clvp: grab if all empty

--- concurrency_control/row_clvp.cpp
+++ concurrency_control/row_clvp.cpp
@@ -91,7 +91,7 @@ RC Row_clvp::lock_get(lock_t type, txn_man * txn, uint64_t* &txnids, int &txncnt
 
 	// check if can grab directly
 	if (retired_cnt == 0 && owner_cnt == 0) {
-		entry->type = entry;
+		entry->type = type;
 		entry->txn = txn;
 		txn->lock_ready = true;
 		rc = RCOK;
