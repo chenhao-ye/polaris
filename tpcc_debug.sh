c7f4205db7d232039ff447a67bba34f751b003fe
scarletg <scarletg@node-2.cc-test.spark-pg0.wisc.cloudlab.us>
2020-01-29 08:20:56 -0600

    silo results

--- tpcc_debug.sh
+++ tpcc_debug.sh
@@ -6,7 +6,7 @@ wl="TPCC"
 threads=16
 cnt=100000
 penalty=0 #1
-wh=1
+wh=4
 spin="true"
 pf="true"
 pf="false"
@@ -15,6 +15,7 @@ debug="false"
 bench="false"
 reorder="false"
 retire="true"
+perc=0.5
 
 for i in 0 1 2
 do
@@ -24,7 +25,7 @@ for wh in 1 2 4 8 16
 do
 for perc in 0 0.5 1
 do
-for alg in CLV WOUND_WAIT WAIT_DIE NO_WAIT
+for alg in SILO #CLV #WOUND_WAIT WAIT_DIE NO_WAIT
 do
 timeout 50 python test.py RETIRE_ON=$retire REORDER_WH=$reorder PERC_PAYMENT=$perc DEBUG_BENCHMARK=$bench DEBUG_CLV=$debug DYNAMIC_TS=$dynamic DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
 done
