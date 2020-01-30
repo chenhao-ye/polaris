6a4196fcbcafeebe60f8afe30d4487e146567b9a
scarletg <scarletg@node-2.cc-test.spark-pg0.wisc.cloudlab.us>
2020-01-30 13:17:41 -0600

    merge hs does not work

--- tpcc_debug.sh
+++ tpcc_debug.sh
@@ -19,9 +19,11 @@ retire="true"
 perc=0.5
 alg="CLV"
 tmp="true"
-on=4
+on=1
+merge="true"
+merge="false"
 
-timeout 30 python test.py CLV_RETIRE_ON=$on DEBUG_TMP=$tmp RETIRE_ON=$retire REORDER_WH=$reorder PERC_PAYMENT=$perc DEBUG_BENCHMARK=$bench DEBUG_CLV=$debug DYNAMIC_TS=$dynamic DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
+timeout 30 python test.py MERGE_HS=$merge CLV_RETIRE_ON=$on DEBUG_TMP=$tmp RETIRE_ON=$retire REORDER_WH=$reorder PERC_PAYMENT=$perc DEBUG_BENCHMARK=$bench DEBUG_CLV=$debug DYNAMIC_TS=$dynamic DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
 
 
 #for i in 0 1 2
