2c565534c7c085b52cdafa9b2a2f878aa43ddf19
scarletg <scarletg@node-0.cc32.spark-pg0.clemson.cloudlab.us>
2020-02-07 15:28:55 -0500

    synthetic

--- synthetic_test.sh
+++ synthetic_test.sh
@@ -18,25 +18,28 @@ read_ratio=1
 phs="true"
 phs="false"
 
-hs=1
+#hs=1
 #pos="TOP"
 #pos="MID"
 #pos="BOT"
-#hs=2
+hs=2
 #pos="TM"
 fhs="WR"
 shs="WR"
 think=0
 
+for i in 0 1
+do
 for penalty in 50000
 do
 for threads in 1 2 4 8 16
 do
-for pos in TOP MID BOT 
+for pos in TM MB #TOP MID BOT 
 do
 for alg in CLV SILO WOUND_WAIT WAIT_DIE
 do
 timeout 50 python test.py THINKTIME=$think PRIORITIZE_HS=$phs READ_PERC=1 NUM_HS=$hs FIRST_HS=$fhs POS_HS=$pos DEBUG_TMP="false" DYNAMIC_TS=$dynamic CLV_RETIRE_ON=$on SPINLOCK=$spin REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic  |& tee -a debug.out
+done
 done
 done
 done
