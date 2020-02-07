c3a1e0367a0925c74bf34ca338cccb0886f8911e
scarletg <scarletg@node-0.cc32.spark-pg0.clemson.cloudlab.us>
2020-02-06 23:08:34 -0500

    rerun clv giving up starvation free

--- tpcc_test.sh
+++ tpcc_test.sh
@@ -5,7 +5,7 @@ rm debug.out
 wl="TPCC"
 threads=16 
 cnt=100000
-penalty=100000 
+penalty=50000 
 wh=1
 spin="true"
 pf="true"
@@ -20,9 +20,11 @@ on=1
 dt=4
 
 
+for penalty in 50000 100000 10000
+do
 for i in 0 1 2
 do
-for delay in 0 10
+for delay in 0 #10
 do
 for threads in 1 2 4 8 16
 do
@@ -37,19 +39,19 @@ done
 done
 done
 done
+done
 
-
-for i in 0 1 2
-do
-for threads in 1 2 4 8 16
-do
-for wh in 1 2 4 8 16
-do
-for alg in SILO WOUND_WAIT WAIT_DIE NO_WAIT
-do
-timeout 60 python test.py DELAY_THRESHOLD=$dt DELAY_ACQUIRE=$delay CLV_RETIRE_ON=$on DEBUG_TMP=$tmp RETIRE_ON=$retire REORDER_WH=$reorder PERC_PAYMENT=$perc DEBUG_BENCHMARK=$bench DYNAMIC_TS=$dynamic DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
+#for i in 0 1 2
+#do
+#for threads in 1 2 4 8 16
+#do
+#for wh in 1 2 4 8 16
+#do
+#for alg in SILO WOUND_WAIT WAIT_DIE NO_WAIT
+#do
+#timeout 60 python test.py DELAY_THRESHOLD=$dt DELAY_ACQUIRE=$delay CLV_RETIRE_ON=$on DEBUG_TMP=$tmp RETIRE_ON=$retire REORDER_WH=$reorder PERC_PAYMENT=$perc DEBUG_BENCHMARK=$bench DYNAMIC_TS=$dynamic DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
 #timeout 50 python test.py RETIRE_ON=$retire REORDER_WH=$reorder PERC_PAYMENT=$perc DEBUG_BENCHMARK=$bench DEBUG_CLV=$debug DYNAMIC_TS=$dynamic DEBUG_PROFILING=$pf SPINLOCK=$spin WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty NUM_WH=${wh}|& tee -a debug.out
-done
-done
-done
-done
+#done
+#done
+#done
+#done
