30bcb2ae680079c69e921c5892cf7d9652c1a28a
kanwu <kanwu@node-1.lock.spark-pg0.clemson.cloudlab.us>
2020-02-19 17:34:28 -0500

    send notfication when done with ycsb test

---
+++ experiments/send_email.py
@@ -0,0 +1,15 @@
+import sys
+import smtplib
+
+
+def send_email(msg):
+    password = 'expnotification@'
+    smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login('experiments.guo@gmail.com',password)
+    smtpObj.sendmail('experiments.guo@gmail.com','scarlet0619@icloud.com',"Subject: {} is done!".format(msg))
+    smtpObj.quit()
+
+if __name__ == "__main__":
+    send_email(sys.argv[1])
