53a0c579ed9afa7a0afb65f143ddfb0fc6488ef8
kanwu <kanwu@node-1.lock.spark-pg0.clemson.cloudlab.us>
2020-02-19 23:46:59 -0500

    ycsb more

--- experiments/ycsb_test.sh
+++ experiments/ycsb_test.sh
@@ -34,11 +34,11 @@ penalty=50000
 
 for i in 0 1 2 3 4
 do
-for alg in CLV #WOUND_WAIT NO_WAIT SILO WAIT_DIE
+for alg in WOUND_WAIT NO_WAIT SILO WAIT_DIE
 do
 for zipf in 0.5 0.7 0.9 1.1
 do
-for read_ratio in 0.1 0.3 0.5 0.7 0.9
+for read_ratio in 0.3 0.7
 do
 timeout 60 python test.py RETIRE_READ=${retire_read} FLIP_RATIO=${flip} SPECIFIED_RATIO=${specified} WW_STARV_FREE=${ww_starv_free} KEY_ORDER=$ordered READ_PERC=${read_ratio} NUM_HS=${num_hs} FIRST_HS=$fhs POS_HS=$pos DEBUG_TMP="false" DYNAMIC_TS=$dynamic CLV_RETIRE_ON=$on SPINLOCK=$spin REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic 
 done
