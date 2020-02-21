6fdec82bdc69db9a7d07f4a7c918116fa37d5cad
kanwu <kanwu@node-1.lock.spark-pg0.clemson.cloudlab.us>
2020-02-21 00:52:50 -0500

    hs1 top all algorithm

--- experiments/hs1_top.sh
+++ experiments/hs1_top.sh
@@ -17,7 +17,6 @@ req=16
 synthetic=true
 zipf=0
 num_hs=1
-pos=SPECIFIED
 pos=TOP
 specified=0
 fixed=1
@@ -37,11 +36,11 @@ penalty=50000
 # figure 4: normalized throughput with optimal case, varying requests
 for i in 0 1 2 3 4
 do
-for alg in WOUND_WAIT
+for alg in CLV WOUND_WAIT WAIT_DIE NO_WAIT SILO
 do
-for threads in 32 #1 2 4 8 16 32
+for threads in 1 2 4 8 16 32
 do
-for req in 64
+for req in 16
 do
 timeout 500 python test.py RETIRE_READ=${retire_read} FIXED_HS=${fixed} FLIP_RATIO=${flip} SPECIFIED_RATIO=${specified} WW_STARV_FREE=${ww_starv_free} KEY_ORDER=$ordered READ_PERC=${read_ratio} NUM_HS=${num_hs} FIRST_HS=$fhs POS_HS=$pos DEBUG_TMP="false" DYNAMIC_TS=$dynamic CLV_RETIRE_ON=$on SPINLOCK=$spin REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic 
 done
@@ -49,4 +48,4 @@ done
 done
 done
 
-python experiments/send_email.py fix_hs1
+python experiments/send_email.py node2_hs1
