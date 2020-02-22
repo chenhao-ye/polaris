d728868a27c74edc806b02ccd8d70742db519321
ScarletGuo <zguo74@wisc.edu>
2020-02-22 16:35:02 -0500

    clean up on server

--- experiments/synthetic_debug.sh
+++ experiments/synthetic_debug.sh
@@ -8,17 +8,18 @@ spin="true"
 ww_starv_free="false"
 # [CLV]
 dynamic="true"
-on=1
+on=0
 retire_read="false"
+retire="false"
 
 # workload
 wl="YCSB"
 req=16
 synthetic=true
 zipf=0
-num_hs=2
+num_hs=1
 pos=SPECIFIED
-specified=0.5
+specified=1
 fixed=1
 fhs="WR"
 shs="WR"
@@ -33,4 +34,4 @@ profile="true"
 cnt=100000 
 penalty=50000
 
-timeout 50 python test_debug.py RETIRE_READ=${retire_read} FIXED_HS=${fixed} FLIP_RATIO=${flip} SPECIFIED_RATIO=${specified} WW_STARV_FREE=${ww_starv_free} KEY_ORDER=$ordered READ_PERC=${read_ratio} NUM_HS=${num_hs} FIRST_HS=$fhs POS_HS=$pos DEBUG_TMP="false" DYNAMIC_TS=$dynamic CLV_RETIRE_ON=$on SPINLOCK=$spin REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic 
+timeout 50 python test_debug.py RETIRE_ON=${false} RETIRE_READ=${retire_read} FIXED_HS=${fixed} FLIP_RATIO=${flip} SPECIFIED_RATIO=${specified} WW_STARV_FREE=${ww_starv_free} KEY_ORDER=$ordered READ_PERC=${read_ratio} NUM_HS=${num_hs} FIRST_HS=$fhs POS_HS=$pos DEBUG_TMP="false" DYNAMIC_TS=$dynamic CLV_RETIRE_ON=$on SPINLOCK=$spin REQ_PER_QUERY=$req DEBUG_PROFILING=$profile SYNTH_TABLE_SIZE=${table_size} WORKLOAD=${wl} CC_ALG=$alg THREAD_CNT=$threads MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty ZIPF_THETA=$zipf SYNTHETIC_YCSB=$synthetic 
