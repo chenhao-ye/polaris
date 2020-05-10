fb763aabc39e70c06d547b5e2bba7f9ffbdb9ddd
scarletg <scarletg@node-0.bamboo.spark-pg0.clemson.cloudlab.us>
2020-05-10 17:21:57 -0400

    seg fault on calling init

--- concurrency_control/row_bamboo.h
+++ concurrency_control/row_bamboo.h
@@ -6,7 +6,7 @@
 
 class Row_bamboo : public Row_bamboo_pt {
  public:
-  void init(row_t * row);
+  void init(row_t * row) override;
   RC lock_get(lock_t type, txn_man * txn, Access * access) override {
     return Row_bamboo_pt::lock_get(type, txn, access);
   };
