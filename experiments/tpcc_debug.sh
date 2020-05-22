1f2697a6b8c3fbdbaa7a105a1bda58a62fb41f6e
scarletg <scarletg@node-0.bamboo.spark-pg0.clemson.cloudlab.us>
2020-05-22 10:12:42 -0400

    ww-pf

--- experiments/tpcc_debug.sh
+++ experiments/tpcc_debug.sh
@@ -25,6 +25,9 @@ penalty=50000
 
 # temp
 latch=LH_MCSLOCK
+user_abort="false"
+cs_pf="false"
+#cnt=1000000
 
 python test_debug.py CC_ALG=$alg LATCH=$latch WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=$dynamic RETIRE_ON=$retire DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} WORKLOAD=${wl} NUM_WH=${wh} PERC_PAYMENT=$perc TPCC_USER_ABORT=${user_abort} THREAD_CNT=$threads DEBUG_PROFILING=${profile} MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
 #
