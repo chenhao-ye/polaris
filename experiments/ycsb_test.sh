96742f99c8ce3787555a5bbf3cae99ebb682e7a5
ScarletGuo <zguo74@wisc.edu>
2020-05-23 15:33:27 -0500

    added new args in sh scripts

--- experiments/ycsb_test.sh
+++ experiments/ycsb_test.sh
@@ -3,13 +3,14 @@ cp -r config-ycsb-std.h config.h
 
 # algorithm
 alg=WOUND_WAIT
-spin="true"
+latch=LH_SPINLOCK
 # [WW]
 ww_starv_free="false"
 # [BAMBOO]
 dynamic="true"
 retire_on="true"
 cs_pf="false"
+opt_raw="true"
 
 # workload
 wl="YCSB"
@@ -33,7 +34,7 @@ profile="true"
 cnt=100000
 penalty=50000
 
-timeout 50 python test.py CC_ALG=${alg} SPINLOCK=${spin} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
+timeout 100 python test.py CC_ALG=${alg} LATCH=${latch} SPINLOCK=${spin} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
 #
 
 cd outputs/
