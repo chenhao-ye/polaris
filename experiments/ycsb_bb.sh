2777cb5265e6adaa996c407b9fc5b6bfb27913d7
scarletg <scarletg@node-0.bamboo.spark-pg0.clemson.cloudlab.us>
2020-05-26 21:21:47 -0400

    ycsb_result

--- experiments/ycsb_bb.sh
+++ experiments/ycsb_bb.sh
@@ -2,8 +2,8 @@ cd ../
 cp -r config-ycsb-std.h config.h
 
 # algorithm
-alg=WOUND_WAIT
-latch=LH_SPINLOCK
+alg=BAMBOO
+latch=LH_MCSLOCK
 # [WW]
 ww_starv_free="false"
 # [BAMBOO]
@@ -23,7 +23,7 @@ specified=0
 fixed=1
 fhs="WR"
 shs="WR"
-read_ratio=1
+read_ratio=0.5
 ordered="false"
 flip=0
 table_size="10000000"
@@ -34,13 +34,25 @@ profile="true"
 cnt=100000
 penalty=50000
 
+for i in 0 1 2 3 4
+do
+for threads in 1 2 4 8 16 32
+do
+for dynamic in true false
+do
+for zipf in 0.5 0.7 0.9 0.99
+do
 timeout 100 python test.py CC_ALG=${alg} LATCH=${latch} SPINLOCK=${spin} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
-#
+done
+done
+done
+done
 
 cd outputs/
 python3 collect_stats.py
-mv stats.csv ycsb/ycsb.csv
-mv stats.json ycsb.json
+mv stats.csv ycsb_bb.csv
+mv stats-summarized.csv ycsb_bb-summarized.csv
+mv stats.json ycsb_bb.json
 cd ..
 
 cd experiments
--- experiments/ycsb_debug.sh
+++ experiments/ycsb_debug.sh
@@ -2,7 +2,7 @@ cp -r config-ycsb-std.h config.h
 
 # algorithm
 alg=$1
-spin="true"
+latch=LH_MCSLOCK
 # [WW]
 ww_starv_free="false"
 # [BAMBOO]
@@ -25,7 +25,7 @@ shs="WR"
 read_ratio=0.9
 ordered="false"
 flip=0
-table_size="1024*1024*10"
+table_size="1000000"
 
 # other
 threads=16
@@ -38,5 +38,5 @@ penalty=50000
 #read_ratio=0.01
 #table_size="1024*10"
 
-python test_debug.py CC_ALG=${alg} SPINLOCK=${spin} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
+python test_debug.py CC_ALG=${alg} LATCH=${latch} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
 #
