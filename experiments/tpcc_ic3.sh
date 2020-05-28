9ee9878cf0744d7acb7fb1352cb06c7186be06c4
scarletg <scarletg@node-0.bamboo.spark-pg0.clemson.cloudlab.us>
2020-05-28 15:47:58 -0400

    com latch

--- experiments/tpcc_ic3.sh
+++ experiments/tpcc_ic3.sh
@@ -30,13 +30,16 @@ penalty=50000
 
 for i in 0 1 2 3 4
 do
-for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
+for com_latcn in true false
+do
+for alg in BAMBOO #SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
 for threads in 1 2 4 8 16 32
 do
 for wh in 1 2 4 8 16 32
 do
 timeout 100 python test.py CC_ALG=$alg LATCH=${latch} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=$dynamic RETIRE_ON=$retire DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} WORKLOAD=${wl} NUM_WH=${wh} PERC_PAYMENT=$perc TPCC_USER_ABORT=${user_abort} COMMUTATIVE_OPS=$com COMMUTATIVE_LATCH=${com_latch} THREAD_CNT=$threads DEBUG_PROFILING=${profile} MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
+done
 done
 done
 done
--- outputs/collect_stats.py
+++ outputs/collect_stats.py
@@ -8,10 +8,12 @@ for line in f:
     data.append(json.loads(line.strip()))
 df = pd.DataFrame(data=data)
 df.to_csv("stats.csv", index=False)
+'''
+df = df.dropna(how='all',axis=1)
 summarized = []
 idx = np.where(df.columns.values == 'abort_cnt')[0][0]
 for cls, group in df.groupby(list(df.columns[:idx])):
     summarized.append(group.loc[group['throughput'].idxmax()])
 summarized = pd.DataFrame(summarized, columns = df.columns).reset_index(drop=True)
 summarized.to_csv("stats-summarized.csv", index=False)
-
+'''
