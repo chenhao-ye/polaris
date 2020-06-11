02108fcabdd5eace1424db0497724875d6d8bf42
scarletg <scarletg@node-0.bamboo.spark-pg0.clemson.cloudlab.us>
2020-06-11 17:01:01 -0400

    pf for aborts

--- experiments/ycsb_debug.sh
+++ experiments/ycsb_debug.sh
@@ -1,7 +1,7 @@
 cp -r config-ycsb-std.h config.h
 
 # algorithm
-alg=BAMBOO
+alg=$1
 latch=LH_MCSLOCK
 # [WW]
 ww_starv_free="false"
@@ -10,7 +10,7 @@ dynamic="true"
 retire_on="true"
 cs_pf="false"
 opt_raw="true"
-max_writer=4
+max_writer=0
 
 # workload
 wl="YCSB"
@@ -36,9 +36,7 @@ penalty=50000
 chain="true"
 
 # temp settings
-dynamic="false"
-threads=$1
-max_writer=$2
+dynamic="true"
 table_size=100000
 
 python test_debug.py CC_ALG=${alg} LATCH=${latch} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_writer} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty DEBUG_ABORT_LENGTH=${chain}
