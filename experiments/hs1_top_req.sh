abd9103487bdaf03325275a2596e6101169440ce
scarletg <scarletg@node-1.bamboo.spark-pg0.clemson.cloudlab.us>
2020-06-17 01:13:28 -0400

    hs1 top 32 threads

--- experiments/hs1_top_req.sh
+++ experiments/hs1_top_req.sh
@@ -1,6 +1,8 @@
 cd ../
 cp -r config-ycsb-synthetic-std.h config.h
 
+fname="hs1_top_req_more"
+
 # algorithm
 alg=WOUND_WAIT
 latch=LH_MCSLOCK
@@ -39,13 +41,13 @@ penalty=50000
 # figure 4: normalized throughput with optimal case, varying requests
 for i in 0 1 2 3 4
 do
-for alg in WOUND_WAIT BAMBOO
+for alg in WOUND_WAIT #BAMBOO
 do
-for threads in 1 2 4 8 16 32
+for threads in 32 #1 2 4 8 16 32
 do
-for req in 4 16 64
+for req in 64 #4 16 64
 do
-timeout 200 python test.py CC_ALG=${alg} LATCH=${latch} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_waiter} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty DEBUG_ABORT_LENGTH=$chain
+timeout 1000 python test.py CC_ALG=${alg} LATCH=${latch} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_waiter} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty DEBUG_ABORT_LENGTH=$chain
 done
 done
 done
@@ -53,8 +55,8 @@ done
 
 cd outputs/
 python3 collect_stats.py
-mv stats.csv hs1_top_req.csv
-mv stats.json hs1_top_req.json
+mv stats.csv ${fname}.csv
+mv stats.json ${fname}.json
 cd ..
 
-python experiments/send_email.py hs1_top_req
+python experiments/send_email.py ${fname}
