7a444694acd2a31c11d64190dbb55b4b9102eb17
scarletg <scarletg@node-1.bb-interactive.spark-pg0.clemson.cloudlab.us>
2020-07-07 16:20:28 -0400

    more zipf

--- experiments/ycsb_zipf.sh
+++ experiments/ycsb_zipf.sh
@@ -1,7 +1,7 @@
 cd ../
 cp -r config-ycsb-std.h config.h
 
-fname="ycsb-zipf"
+fname="ycsb-zipf-bb-more"
 # algorithm
 latch=LH_MCSLOCK
 # [WW]
@@ -37,16 +37,13 @@ cnt=100000
 penalty=50000
 chain="false"
 
-dynamic="false"
 read_ratio=0.5
 numa="true"
-for latch in LH_MCSLOCK #LH_SPINLOCK
-do
-for i in 0 1 2 3 4
-do
 for alg in BAMBOO #WOUND_WAIT SILO WAIT_DIE NO_WAIT 
 do
 for zipf in 0.5 0.6 0.7 0.8 0.9 0.99
+do
+for i in 0 1 2 3 4 5 6 7 8 
 do
 if [ $alg == "BAMBOO" ] 
 then
@@ -54,14 +51,16 @@ for last_retire in 0 0.15
 do
 for dynamic in true false
 do
+for opt_raw in true false
+do
 timeout 200 python test.py UNSET_NUMA=${numa} CC_ALG=${alg} LATCH=${latch} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_waiter} LAST_RETIRE=${last_retire} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty DEBUG_ABORT_LENGTH=${chain}
+done 
 done 
 done
 else
 last_retire=0 
 timeout 200 python test.py UNSET_NUMA=${numa} CC_ALG=${alg} LATCH=${latch} WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=${dynamic} RETIRE_ON=${retire_on} DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_waiter} LAST_RETIRE=${last_retire} WORKLOAD=${wl} REQ_PER_QUERY=$req SYNTHETIC_YCSB=$synthetic ZIPF_THETA=$zipf NUM_HS=${num_hs} POS_HS=$pos SPECIFIED_RATIO=${specified} FIXED_HS=${fixed} FIRST_HS=$fhs SECOND_HS=$shs READ_PERC=${read_ratio} KEY_ORDER=$ordered FLIP_RATIO=${flip} SYNTH_TABLE_SIZE=${table_size} THREAD_CNT=$threads DEBUG_PROFILING=$profile MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty DEBUG_ABORT_LENGTH=${chain}
 fi
-done
 done
 done
 done
