094f5a1e13e7847ec0bf808764afc7a07ec8c9c7
ScarletGuo <scarletg@node-0.bamboo.spark-pg0.clemson.cloudlab.us>
2020-09-03 00:36:09 -0400

    update test script

--- experiments/tpcc_debug.sh
+++ experiments/tpcc_debug.sh
@@ -25,8 +25,5 @@ profile="true"
 cnt=100000
 penalty=50000 
 
-## tmp 
-com="true"
-
 python test_debug.py CC_ALG=$alg LATCH=$latch WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=$dynamic RETIRE_ON=$retire DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_waiter} WORKLOAD=${wl} NUM_WH=${wh} PERC_PAYMENT=$perc TPCC_USER_ABORT=${user_abort} COMMUTATIVE_OPS=$com THREAD_CNT=$threads DEBUG_PROFILING=${profile} MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
 #
