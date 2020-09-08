a0d120dfe8b0cb930e1dee6858348f4d38e790f0
ScarletGuo <scarletg@node-0.bamboo.spark-pg0.clemson.cloudlab.us>
2020-09-08 00:03:43 -0400

    ic3 with optimization

--- experiments/tpcc_ic3.sh
+++ experiments/tpcc_ic3.sh
@@ -1,7 +1,7 @@
 cd ../
 rm outputs/stats.json
 cp -r config-std.h config.h
-fname="tpcc_ic3_base"
+fname="tpcc_ic3_opt"
 
 ## algorithm
 alg=BAMBOO
@@ -14,11 +14,14 @@ retire="true"
 cs_pf="false"
 opt_raw="true"
 max_waiter=0
+# ic3
+ic3_eager="true"
+ic3_rd="true"
 
 ## workload
 wl="TPCC"
 wh=2
-perc=1 # payment percentage
+perc=0.5 # payment percentage
 user_abort="true"
 com="false"
 
@@ -30,15 +33,15 @@ penalty=50000
 
 for i in 0 1 2 3 4
 do
-for alg in IC3 BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
+for alg in IC3 BAMBOO #SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
 for threads in 1 2 4 8 16 32
 do
 for wh in 1 2 4 8 16
 do
-for perc in 0 0.5 1
+for com in true false 
 do
-timeout 100 python test.py CC_ALG=$alg LATCH=$latch WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=$dynamic RETIRE_ON=$retire DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_waiter} WORKLOAD=${wl} NUM_WH=${wh} PERC_PAYMENT=$perc TPCC_USER_ABORT=${user_abort} COMMUTATIVE_OPS=$com THREAD_CNT=$threads DEBUG_PROFILING=${profile} MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
+timeout 200 python test.py CC_ALG=$alg LATCH=$latch WW_STARV_FREE=${ww_starv_free} DYNAMIC_TS=$dynamic RETIRE_ON=$retire DEBUG_CS_PROFILING=${cs_pf} BB_OPT_RAW=${opt_raw} BB_OPT_MAX_WAITER=${max_waiter} IC3_EAGER_EXEC=${ic3_eager} IC3_RENDEZVOUS=${ic3_rd} WORKLOAD=${wl} NUM_WH=${wh} PERC_PAYMENT=$perc TPCC_USER_ABORT=${user_abort} COMMUTATIVE_OPS=$com THREAD_CNT=$threads DEBUG_PROFILING=${profile} MAX_TXN_PER_PART=$cnt ABORT_PENALTY=$penalty
 done
 done
 done
