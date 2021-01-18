83e6c93191abbf06c7c6a9c24ed72b7b4f03b49a
ScarletGuo <zguo74@wisc.edu>
2021-01-18 13:49:48 -0800

    set up exp for large # thds and large dataset

--- experiments/ycsb_thd.sh
+++ experiments/ycsb_thd.sh
@@ -1,21 +1,18 @@
 cd ..
 
-for zipf in 0.9 #0 0.6 0.99
+for i in 0 1 2 3 4
+for zipf in 0.9
 do
-for thd in 1 2 4 8 16 32
+for thd in 1 2 4 8 16 32 64 96 128
 do
 for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
-		python test.py experiments/default.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} COMPILE_ONLY=true
-		for i in 0 1 2 3 4
-		do
-			python test.py experiments/default.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} EXEC_ONLY=true OUTPUT_TO_FILE=true 
-		done
+		python test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true
 done
 done
 done
 
-fname="ycsb_thd_9"
+fname="ycsb_thd_large"
 cd outputs/
 python3 collect_stats.py
 mv stats.csv ${fname}.csv
