9b40f12e0d4a89f5c20c7525ee6d8fdd81e2f583
ScarletGuo <zguo74@wisc.edu>
2021-01-19 16:38:02 -0800

    setup large dataset and high core count

--- experiments/ycsb_thd.sh
+++ experiments/ycsb_thd.sh
@@ -3,11 +3,11 @@ cd ..
 for i in 0 1 2 3 4
 for zipf in 0.9
 do
-for thd in 1 2 4 8 16 32 64 96 128
+for thd in 1 2 4 8 16 32 64 96 
 do
 for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
-		python test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true
+		python test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8
 done
 done
 done
