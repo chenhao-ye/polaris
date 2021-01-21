f5b0d5e07416eb210790b183f0b782c906ebbb27
ScarletGuo <zguo74@wisc.edu>
2021-01-21 05:59:42 -0800

    partial results on long txn

--- experiments/ycsb_long_txn.sh
+++ experiments/ycsb_long_txn.sh
@@ -1,16 +1,14 @@
 cd ..
 rm outputs/stats.json
 
+zipf=0.9
 for i in 0 1 2 3 4
-do
-for zipf in 0.9
 do
 for thd in 1 2 4 8 16 32 64
 do
 for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
 		python test.py experiments/long_txn.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8 
-done
 done
 done
 done
