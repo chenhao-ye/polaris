bc769a8f941fba121eb6cde06b953b4b9ab9fb46
ScarletGuo <zguo74@wisc.edu>
2021-01-20 21:52:19 -0800

    ycsb-thd-large

--- experiments/ycsb_thd.sh
+++ experiments/ycsb_thd.sh
@@ -1,6 +1,7 @@
 cd ..
 
 for i in 0 1 2 3 4
+do
 for zipf in 0.9
 do
 for thd in 1 2 4 8 16 32 64 96 
@@ -8,6 +9,7 @@ do
 for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
 		python test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8
+done
 done
 done
 done
