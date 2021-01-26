966a8a6adc31e274f1a88d2f52183fb476f50047
ScarletGuo <zguo74@wisc.edu>
2021-01-26 14:19:50 -0800

    partial results on ycsb long high core counts

--- experiments/ycsb_long_txn_ww.sh
+++ experiments/ycsb_long_txn_ww.sh
@@ -1,14 +1,14 @@
 cd ..
-cp outputs/ycsb_long_txn.json outputs/stats.json
+cp outputs/ycsb_long_txn_large_core.json outputs/stats.json
 
 zipf=0.9
 for i in 0 1 2 3 4
 do
-for thd in 64 #1 2 4 8 16 32 64
+for thd in 64 96 120 #1 2 4 8 16 32 64
 do
-for alg in WOUND_WAIT 
+for alg in WAIT_DIE 
 do
-		python test.py experiments/long_txn.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8 MAX_RUNTIME=10
+		python test.py experiments/long_txn.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8 MAX_RUNTIME=5
 done
 done
 done
