2f1699bdb9df08fb6e5092c6e14628976d711336
ScarletGuo <zguo74@wisc.edu>
2021-02-02 16:30:48 -0800

    reset exp for ycsb thd

--- experiments/ycsb_thd.sh
+++ experiments/ycsb_thd.sh
@@ -1,21 +1,19 @@
 cd ..
-cp outputs/ycsb_thd_100g.json outputs/stats.json
+rm outputs/stats.json
 
+zipf=0.9
+for thd in 1 2 4 8 16 32 64 96 120
+do
 for i in 0 1 2 3 4
 do
-for zipf in 0.9
-do
-for thd in 120 #1 2 4 8 16 32 64 96 
-do
-for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
+for alg in BAMBOO #SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
 		python test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8
 done
 done
 done
-done
 
-fname="ycsb_thd_100g_high"
+fname="ycsb_thd_100g_bamboo"
 cd outputs/
 python3 collect_stats.py
 mv stats.csv ${fname}.csv
