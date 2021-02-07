5a40cfbe700591cf0cb19dc44649193de614bdac
ScarletGuo <zguo74@wisc.edu>
2021-02-06 21:00:38 -0800

    rerun skipping wait die

--- experiments/ycsb_long_txn.sh
+++ experiments/ycsb_long_txn.sh
@@ -1,12 +1,12 @@
 cd ..
-rm outputs/stats.json
+#rm outputs/stats.json
 
 zipf=0.9
 for i in 0 1 2 3 4
 do
-for thd in 120 96 64 32 16 8 4 2 1 # 1 2 4 8 16 32 64 96 120 
+for thd in 8 4 2 1 120 96 64 32 16 #8 4 2 1 # 1 2 4 8 16 32 64 96 120 
 do
-for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
+for alg in BAMBOO SILO WOUND_WAIT NO_WAIT #WAIT_DIE
 do
 		python test.py experiments/long_txn.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8 
 done
