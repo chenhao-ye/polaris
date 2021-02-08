4c910aafcf9f46fd27a8c2deee90722fc5a2cd7f
ScarletGuo <zguo74@wisc.edu>
2021-02-08 08:26:35 -0800

    woundwait rerun

--- experiments/ycsb_thd.sh
+++ experiments/ycsb_thd.sh
@@ -6,18 +6,18 @@ for i in 0 1 2 3 4
 do
 for thd in 120 96 64 32 16 8 4 2 1 #1 2 4 8 16 32 64 96 #120
 do
-for alg in BAMBOO #SILO WOUND_WAIT WAIT_DIE NO_WAIT
+for alg in WOUND_WAIT #SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
 		python test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8
 done
 done
 done
 
-fname="ycsb_thd_100g_bamboo"
+fname="ycsb-thd-100g-woundwait"
 cd outputs/
 python3 collect_stats.py
-mv stats.csv ${fname}.csv
-mv stats.json ${fname}.json
+mv stats.csv ycsb_thd/${fname}.csv
+mv stats.json ycsb_thd/${fname}.json
 cd ..
 
 cd experiments
