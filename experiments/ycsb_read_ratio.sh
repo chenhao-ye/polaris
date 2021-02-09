01d773224c69ba40daac153980b76459daeb313b
ScarletGuo <zguo74@wisc.edu>
2021-02-08 22:36:25 -0800

    ycsb thd rerun other algs

---
+++ experiments/ycsb_read_ratio.sh
@@ -0,0 +1,25 @@
+cd ..
+rm outputs/stats.json
+
+thd=32
+zipf=0.9
+for i in 0 1 2 3 4
+do
+for ratio in 0.1 0.3 0.5 0.7 0.9 
+do
+for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
+do
+		python test.py experiments/large_dataset.json READ_PREC=${ratio} THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8
+done
+done
+done
+
+fname="ycsb-read-ratio-100g"
+cd outputs/
+python3 collect_stats.py
+mv stats.csv ycsb_other/${fname}.csv
+mv stats.json ycsb_other/${fname}.json
+cd ..
+
+cd experiments
+python3 send_email.py ${fname}
