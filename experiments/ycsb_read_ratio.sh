6389c181c8caa3b8a106be11e0cfd5513b411f05
ScarletGuo <zguo74@wisc.edu>
2021-02-09 18:53:29 -0800

    tpcc-thd

--- experiments/ycsb_read_ratio.sh
+++ experiments/ycsb_read_ratio.sh
@@ -1,15 +1,15 @@
 cd ..
-rm outputs/stats.json
+cp outputs/ycsb_other/ycsb-read-ratio-100g.json outputs/stats.json
 
 thd=32
 zipf=0.9
 for i in 0 1 2 3 4
 do
-for ratio in 0.1 0.3 0.5 0.7 0.9 
+for ratio in 0.1 0.3 0.7 0.9 #0.5
 do
 for alg in BAMBOO SILO WOUND_WAIT WAIT_DIE NO_WAIT
 do
-		python test.py experiments/large_dataset.json READ_PREC=${ratio} THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8
+		python test.py experiments/large_dataset.json READ_PERC=${ratio} THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} OUTPUT_TO_FILE=true CPU_FREQ=2.8
 done
 done
 done
