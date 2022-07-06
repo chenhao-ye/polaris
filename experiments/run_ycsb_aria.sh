f8c3d7d1fed3a035fc0aefb47391e29a5e3c2a77
Chenhao Ye <ye.chenhao.ych@gmail.com>
2022-07-06 08:51:07 -0500

    make aria experiments longer for stable results

--- experiments/run_ycsb_aria.sh
+++ experiments/run_ycsb_aria.sh
@@ -1,3 +1,6 @@
+# Note: it turns out p999 of ARIA can vary a lot; to get a relatively stable
+# results, we make experiment duration longer and dump all latency
+
 exper=ycsb_aria_batch
 mkdir -p results
 
@@ -7,11 +10,7 @@ for zipf in 0.99 0.5; do
 		for batch in 1 2 4 8; do
 			data_dir="results/${exper}/YCSB-CC=${alg}_${batch}-THD=${thd}-ZIPF=${zipf}"
 			mkdir -p "${data_dir}"
-			if [ "$thd" != "64" ] && [ "$thd" != "16" ]; then
-				python3 test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} ARIA_BATCH_SIZE=${batch} | tee "${data_dir}/log"
-			else
-				python3 test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} ARIA_BATCH_SIZE=${batch} DUMP_LATENCY=true DUMP_LATENCY_FILENAME="\"${data_dir}/latency_dump.csv\"" | tee "${data_dir}/log"
-			fi
+			python3 test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} ARIA_BATCH_SIZE=${batch} DUMP_LATENCY=true DUMP_LATENCY_FILENAME="\"${data_dir}/latency_dump.csv\"" MAX_RUNTIME=40 | tee "${data_dir}/log"
 		done
 	done
 done
@@ -22,11 +21,7 @@ for zipf in 0.99 0.5; do
 	for thd in 1 4 8 16 24 32 40 48 56 64; do
 		data_dir="results/${exper}/YCSB-CC=${alg}-THD=${thd}-ZIPF=${zipf}"
 		mkdir -p "${data_dir}"
-		if [ "$thd" != "64" ] && [ "$thd" != "16" ]; then
-			python3 test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} | tee "${data_dir}/log"
-		else
-			python3 test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} DUMP_LATENCY=true DUMP_LATENCY_FILENAME="\"${data_dir}/latency_dump.csv\"" | tee "${data_dir}/log"
-		fi
+		python3 test.py experiments/large_dataset.json THREAD_CNT=${thd} ZIPF_THETA=${zipf} CC_ALG=${alg} DUMP_LATENCY=true DUMP_LATENCY_FILENAME="\"${data_dir}/latency_dump.csv\"" MAX_RUNTIME=40 | tee "${data_dir}/log"
 	done
 done
 
